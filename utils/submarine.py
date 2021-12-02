2a1038d7ee278d238045c3b18b284978408d1927
Spencer Leake <spencer.leake@gmail.com>
2021-12-02 08:22:28 -0500

    Updated day2 util for depth instead of y

--- utils/submarine.py
+++ utils/submarine.py
@@ -1,19 +1,19 @@
 def trackPosition(inputs):
-    x,y = 0,0
+    x,depth = 0,0
     for inputdata in inputs:
         direction,distance = inputdata.split()
         x += (direction=='forward')*int(distance)
-        y += (direction=='up')*int(distance)
-        y -= (direction=='down')*int(distance)
-    return x,y
+        depth -= (direction=='up')*int(distance)
+        depth += (direction=='down')*int(distance)
+    return x,depth
         
 
 def trackCorrectPosition(inputs):
-    x,y,aim = 0,0,0
+    x,depth,aim = 0,0,0
     for inputdata in inputs:
         direction,distance = inputdata.split()
-        aim += (direction=='up')*int(distance)
-        aim -= (direction=='down')*int(distance)
+        aim -= (direction=='up')*int(distance)
+        aim += (direction=='down')*int(distance)
         x += (direction=='forward')*int(distance)
-        y += (direction=='forward')*int(distance)*aim
-    return x,y
+        depth += (direction=='forward')*int(distance)*aim
+    return x,depth
