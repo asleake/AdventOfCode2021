238c37ed269f23dae9d699337254726d67d497d4
Spencer Leake <spencer.leake@gmail.com>
2021-12-02 08:18:38 -0500

    Day 03 prepped

---
+++ day03.py
@@ -0,0 +1,18 @@
+from utils.import_data import getFileName,getInputFromFile
+
+data = getInputFromFile('data/'+getFileName(__file__)+'.input')
+
+
+
+# Solve Part 1
+part1 = 'This is not the answer yet.'
+
+
+# Solve Part 2
+part2 = 'This is not the answer yet.'
+
+
+# Output answers
+print('Part 1:\n'+str(part1))
+print('\n\n')
+print('Part 2:\n'+str(part2))
