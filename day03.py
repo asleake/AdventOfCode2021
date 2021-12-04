32cb82b6a6b9652cbd4408875c4724d878cb34cc
Spencer Leake <spencer.leake@gmail.com>
2021-12-03 21:55:55 -0500

    Day 03 complete

--- day03.py
+++ day03.py
@@ -1,19 +1,23 @@
 from utils.import_data import getFileName,getInputFromFile
-
+from utils import submarine
+from math import prod
 data = getInputFromFile('data/'+getFileName(__file__)+'.input')
 
-
+sub = submarine.Submarine()
 
 # Solve Part 1
 def FirstPart():
     """ Find ... """
-    return 'This is not the answer yet.'
+
+    sub.calculateDiagnostic(data)
+    return sub.gamma*sub.epsilon
 
 
 # Solve Part 2
 def SecondPart():
     """ Find ... """
-    return 'This is not the answer yet.'
+    sub.calculateLifeSupportRating(data)
+    return sub.oxygenRating*sub.co2Rating
 
 
 # Output answers
